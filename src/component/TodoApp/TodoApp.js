e2178027dd77407e45a7e82d8cd94a3bfa98fedc
fuad <ahmadufuadoorakam@gmail.com>
2024-06-24 07:19:55 +0530

    created todo list setups

--- src/component/TodoApp/TodoApp.js
+++ src/component/TodoApp/TodoApp.js
@@ -2,16 +2,45 @@ import React, { Component } from "react";
 import "./TodoApp.css";
 
 export default class TodoApp extends Component {
+
+  state = {
+    inputValue: "",
+    items: []
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      inputValue: event.target.value
+    })
+  }
+
+  storeItems = event => {
+    event.preventDefault()
+    const { inputValue } = this.state;
+
+    this.setState({
+      items: [...this.state.items, inputValue],
+      inputValue: ""
+    });
+  };
+
   render() {
+    const { inputValue, items } = this.state;
+
     return (
       <div className="todo-container">
-        <form className="input-section">
+        <form className="input-section" onSubmit={this.storeItems}>
           <h1>Todo App</h1>
-          <input type="text" placeholder="Enter Items...." />
+          <input value={inputValue} onChange={this.handleChange} type="text" placeholder="Enter Items...." />
         </form>
         <ul>
-          <li>Item <i className="fa-solid fa-trash-can"></i></li>
-          <li>Item <i className="fa-solid fa-trash-can"></i></li>
+          {items.map((data, index) => {
+            return (
+              <li key={index}>
+                {data} <i className="fa-solid fa-trash-can"></i>
+              </li>
+            );
+          })}
         </ul>
       </div>
     );
