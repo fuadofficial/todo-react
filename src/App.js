e83027cb797818f50d68ad99c722c7ab61157ed2
fuad <ahmadufuadoorakam@gmail.com>
2024-06-26 06:52:03 +0530

    created a router methods

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import TodoApp from "./component/TodoApp/TodoApp";
+import Header from "./component/Header/Header";
+import About from "./component/TodoApp/About/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <div>
-      <TodoApp />
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<TodoApp />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
